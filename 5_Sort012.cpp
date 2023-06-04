ebc3ef981e8da3abf37f2aa17ce966a5fcbfbdbb
Jyoti-prakash-Nayak <87771215+Jyoti-prakash-Nayak@users.noreply.github.com>
2023-06-04 23:16:45 +0530

    Create Sort 0 1 2

---
+++ 5_Sort012.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   
+   int low=0;
+   int mid=0;
+   int high=n-1;
+   while(mid<=high){
+      if(arr[mid]==0){
+         swap(arr[low++],arr[mid++]); 
+      }
+      else if(arr[mid]==2){
+         swap(arr[mid],arr[high--]);
+      }
+      else{
+         mid++;
+      }
+   }
+}
