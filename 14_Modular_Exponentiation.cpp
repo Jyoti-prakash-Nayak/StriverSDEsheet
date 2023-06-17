5fbe343ac08c7437d15999f4bde0ff9114d8cec2
Jyoti-prakash-Nayak <87771215+Jyoti-prakash-Nayak@users.noreply.github.com>
2023-06-17 16:49:05 +0530

    Create Modular Exponentiation

---
+++ 14_Modular_Exponentiation.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int modularExponentiation(int x, int n, int m) {
+	// Write your code here.
+	int ans=1;
+	while(n){
+		if(n&1)
+		  ans=(1LL * ans * (x)%m )%m;
+		  x=(1LL * (x)%m * (x)%m )%m;
+		  n>>=1;
+	}
+	return ans;
+}
