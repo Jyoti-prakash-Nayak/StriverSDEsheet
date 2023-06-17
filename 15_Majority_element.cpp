54c9118981a46625c456f3109ed9fb19f3f4d293
Jyoti-prakash-Nayak <87771215+Jyoti-prakash-Nayak@users.noreply.github.com>
2023-06-17 16:50:48 +0530

    Create Majority element

---
+++ 15_Majority_element.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int findMajorityElement(int arr[], int n) {
+	// Write your code here.
+	unordered_map<int,int>mp;
+	for(int i=0;i<n;i++){
+		mp[arr[i]]++;
+	}
+	for(int i=0;i<n;i++){
+		if(mp[arr[i]] > n/2){
+			return arr[i];
+		}
+	}
+	return -1;
+}
