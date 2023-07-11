edda4dada993eea93f6f4ef149cae7517539911f
Jyoti-prakash-Nayak <87771215+Jyoti-prakash-Nayak@users.noreply.github.com>
2023-07-11 11:26:23 +0530

    Create Next Smaller Element

---
+++ 89_Next_Smaller_Element.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    // Write your code here.
+    vector<int> res(n, -1);
+    stack<int> st;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (!st.empty())
+        {
+            if (st.top() < arr[i])
+            {
+                res[i] = st.top();
+                break;
+            }
+            st.pop();
+        }
+        st.push(arr[i]);
+    }
+    return res;
+}
